2a2233ee026ccbfa3b15b8abf4f75461a55dc0a2
liust <pingtoulou@gmail.com>
2017-03-09 09:08:00 +0800

    newTest
    test

---
+++ liust-util/src/com/liust/util/Log/Test.java
@@ -0,0 +1,13 @@
+package com.liust.util.Log;
+
+import java.util.Objects;
+
+/**
+ * Created by liust on 17/2/24.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println(" Hello World !!");
+        System.out.println(Objects.equals(null,null));
+    }
+}
